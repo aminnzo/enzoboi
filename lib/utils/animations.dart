88c88502ea45aaea221b4a28cde12d60c5afc871
enzo <dartmolecules@gmail.com>
2023-01-04 15:33:09 +0330

    capsulate animations

---
+++ lib/utils/animations.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const animationDuration = Duration(milliseconds: 1400);
+
+Animation<Offset> getTweenAnimation(
+    {double beginX = 0.0,
+    double beginY = 0.0,
+    double endX = 0.0,
+    double endY = 0.0,
+    double begin = 0,
+    double end = 1,
+    Curve cubic = Curves.easeOutQuint,
+    required AnimationController controller}) {
+  return Tween<Offset>(
+    begin: Offset(beginX, beginY),
+    end: Offset(endX, endY),
+  ).animate(
+    CurvedAnimation(
+      parent: controller,
+      curve: Interval(begin, end, curve: cubic),
+    ),
+  );
+}
